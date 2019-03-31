19ceb3b7a6fcad92a938faa1fa9f40141c67b795
Junior Dugue <jrdugue@gmail.com>
2019-03-31 15:05:22 -0400

    created tosscoin function and made it output via console to show which button was clicked heads or tails

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Coin-Toss-JS-App
+
+Simple coin toss app to practice js DOM
--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const message = document.querySelector(".message");
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
+
+for (let i=0; i<buttons.length; i++){
+  console.log(buttons[i]);
+  buttons[i].addEventListener("click",tossCoin);
+}
+
+function tossCoin(e){
+  console.log(e.target.innerText);
+}
+
